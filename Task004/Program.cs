2ee818d9e6e82b203ba776925ca73f14ad7a9a85
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-20 21:54:56 +0300

    add Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,15 @@
+﻿// 4. Найти максимальное из трех чисел
+int a = 65;
+int b = 99;
+int c = 9;
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.Write("Максимальное число ");
+Console.WriteLine(max);
