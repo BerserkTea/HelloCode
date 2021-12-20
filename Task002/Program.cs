1aa8fe7870a48fe2878c82f53a4f80dd364cd48e
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-20 21:12:52 +0300

    add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = 116;
+int e = 5;
+
+if(x>e)
+{
+    Console.WriteLine("Наибольшим является ");
+    Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine("Наибольшим является ");
+    Console.WriteLine(e);
+}
