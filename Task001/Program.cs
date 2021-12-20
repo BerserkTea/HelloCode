2b3f26021066cb2f85808eefbb5be454658b8589
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-20 21:00:12 +0300

    add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// По двум заданным числам проверять является ли первое квадратом второго
+
+int a = 4;
+int b = 5;
+a = a*a;
+if (a==b)
+{
+    Console.WriteLine("Число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Число не является квадратом второго");
+}
