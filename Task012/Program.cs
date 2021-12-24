26367750119175992addb1e7a0f6ca9da044f427
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-24 23:05:03 +0300

    Add solution to Task012

---
+++ Task012/Program.cs
@@ -0,0 +1,14 @@
+﻿//12. Удалить вторую цифру трёхзначного числа
+int a = 321;
+
+string numberString = Convert.ToString(a);
+
+if (numberString.Length>3)
+{
+    Console.WriteLine("длинна превышена");
+}
+else
+{
+    string numberlessString = numberString.Remove(1,1); // создаем новую переменную у которой удаляем 2ой символ(номерация с нуля), но тут переменная в формате string
+    Console.WriteLine(numberlessString);
+}
