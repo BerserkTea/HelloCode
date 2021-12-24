3a76981b5c7853dafaf90ba5f05acd4743531a59
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-24 23:11:28 +0300

    Add solution to Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,14 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+double a = 6;
+double b = 64;
+
+if (b%a==0)
+{
+    Console.WriteLine("Число b кратно числу a");
+}
+else
+{
+    double x=b%a;
+    Console.Write("Число b не кратно числу a, и остаток составляет ");
+    Console.WriteLine(x);
+}
