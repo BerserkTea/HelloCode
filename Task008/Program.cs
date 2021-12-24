9dc38dd3c32e34d9fe64ca521cd99336f183e821
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-24 14:06:21 +0300

    Add solution to Task008

---
+++ Task008/Program.cs
@@ -0,0 +1,13 @@
+﻿// 8. Показать четные числа от 1 до N
+Console.WriteLine("Hello, World!");
+
+int a = 16;
+int count = 1;
+while (count <= a)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
