e5bd6e052bbef8acbd6754e518bd654ed0bba2bf
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-24 12:26:41 +0300

    Add new solution task 005

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -1,5 +1,14 @@
 ﻿// 5. Написать программу вычисления значения функции y = f(a)
-double x = 5.4;
-double y = 9999;
-y = x*x*(x + x);
-Console.WriteLine(y);
+//double x = 5.4;
+//double y = 9999;
+//y = x*x*(x + x);
+//Console.WriteLine(y);
+
+double method1 (double a)
+{
+    double result = a*a+1;
+    return result;
+}
+double v = method1(9);
+Console.WriteLine("Функция y = f(a) при а = 9 равна ");
+Console.WriteLine(v);
