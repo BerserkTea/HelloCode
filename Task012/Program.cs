bf41248fa48134dcd878a6e0f6da1254a0c36310
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-26 13:07:13 +0300

    added the text of the problem, corrected typos 012

--- Task012/Program.cs
+++ Task012/Program.cs
@@ -12,9 +12,9 @@
 //     string numberlessString = numberString.Remove(1,1); // создаем новую переменную у которой удаляем 2ой символ(номерация с нуля), но тут переменная в формате string
 //     Console.WriteLine(numberlessString);
 // }
-
+Console.WriteLine("Удалить вторую цифру трёхзначного числа");
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int result1 = number  %  10;
 int result2 = (number  % 1000) / 100;
-Console.WriteLine("После удаления второй числы числа получился следующий результат: " + result2 + result1);
+Console.WriteLine("После удаления второй цифры числа получился следующий результат: " + result2 + result1);
