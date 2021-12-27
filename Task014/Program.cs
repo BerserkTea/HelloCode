98f87643e07fe0791f51e107fd5131dcacde0dee
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-27 23:57:31 +0300

    Add new (hope last)  solution Task 014

--- Task014/Program.cs
+++ Task014/Program.cs
@@ -28,12 +28,25 @@
 Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (100 > a )
+if (99 > a)
 {
-    Console.WriteLine("ошибка"); 
+    Console.WriteLine("ошибка 3 цифры нет");
 }
 else
 {
-    int y = a%10;
-    Console.WriteLine(y);
+    string y = Convert.ToString(a);     // создал переменную стринг конвертацией int из переменной а
+    // Console.WriteLine(y[2]);
+    //Console.WriteLine(y.Length);
+    int[] numbers = new int[y.Length];  // Создаю массив с длинной стоки y
+    int Z = a;                          // Ввел переменную Z что бы не пилить стартовую переменную а.
+    int arrSize = y.Length;             // ввел переменную длинны исходя из длинны строки y
+    for (int i = 0; Z > 0; i++)
+    {
+        numbers[arrSize - 1] = Z % 10;  // Берем последний элемент массива(а не первый, и записываем в него остаток от деления числа Z на 10)
+        arrSize = arrSize - 1;          // уменьшаем счетчик длинны на 1 что бы в след раз вносить уже в другой номер элемента массива
+        //Console.WriteLine(arrSize);
+        Z = Z / 10;                     // уменьшаем сходное число убирая у него последнее число, что бы в след проходе работать уже с другими числами.
+        //Console.WriteLine(numbers[i]);
+    }
+    Console.WriteLine(numbers[2]);
 }
