b4258bd4c95e0f6a50506b64b14072451c128e6c
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-30 21:58:43 +0300

    Add new  Task 017-019

---
+++ Task017/Program.cs
@@ -0,0 +1,14 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Введите число 1 ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+double sqrFirstNumber = firstNumber*firstNumber;
+double sqrSecondNumber = secondNumber*secondNumber;
+
+if (sqrFirstNumber == secondNumber);
+
+//смотри 1 задачу
---
+++ Task018/Program.cs
@@ -0,0 +1,22 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+
+//пишем метод
+
+bool check(bool x, bool y)
+{
+    if (!(x || y) == (!x && !y))
+        return true;
+    else
+        return false;
+}
+
+if (check(false, false) && check(false, true) && check(true, false) && check(true, true))
+    Console.WriteLine("Верно");
+else
+{
+    Console.WriteLine("Не Верно");
+}
+
+
+
