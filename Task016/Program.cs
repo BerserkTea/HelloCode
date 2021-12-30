5f5797d2c2283fcdc9011a13211a0d51c67f11bd
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-30 20:50:57 +0300

    Add new || Task 016

--- Task016/Program.cs
+++ Task016/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Введите число дня недели");
 int a = Convert.ToInt32(Console.ReadLine())-1;
 if (a>-1 && a<7)
 {
-    if (a==5 | a==6)
+    if (a==5 || a==6)
     {
         Console.WriteLine(dayOfWeek[a]+" Выходной, чилим изи");
     }
