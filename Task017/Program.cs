b7a3117613ba5a567976c56d74a61f3cb04d2424
«AndronovIvan» <andronoviv4@gmail.com>
2021-12-30 22:42:41 +0300

    Add solution Task017

--- Task017/Program.cs
+++ Task017/Program.cs
@@ -6,9 +6,19 @@ int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-double sqrFirstNumber = firstNumber*firstNumber;
-double sqrSecondNumber = secondNumber*secondNumber;
+int sqrFirstNumber = firstNumber * firstNumber;
+int sqrSecondNumber = secondNumber * secondNumber;
 
-if (sqrFirstNumber == secondNumber);
-
+if (sqrFirstNumber == secondNumber)
+{
+    Console.WriteLine($"Число {secondNumber} является квадратом числа {firstNumber}");
+}
+else if (sqrSecondNumber == firstNumber)
+{
+    Console.WriteLine($"Число {firstNumber} является квадратом числа {secondNumber}");
+}
+else
+{
+    Console.WriteLine ("Числа не являются квадратом друг друга");
+}
 //смотри 1 задачу
