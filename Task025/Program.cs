7be2b207c2fc0a82834bb9cdaf260b1b8a476113
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-08 13:50:40 +0300

    Add math formula solution Task025

---
+++ Task025/Program.cs
@@ -0,0 +1,5 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите число ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел от 1 до {NumberA} равна {(NumberA+1)*NumberA/2}");
