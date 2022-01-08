62923def6b50319f7ba62cf9b9d8fc42264a5bd2
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-08 14:50:52 +0300

    Add solution Task026

---
+++ Task026/Program.cs
@@ -0,0 +1,24 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите число ");
+double NumberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите степень для возведения числа ");
+int degreeB = Convert.ToInt32(Console.ReadLine());
+double degree = 1;
+if (NumberA == 0)
+{
+    Console.WriteLine(0);
+}
+else if (degreeB == 0)
+{
+    Console.WriteLine(1);
+}
+else
+{
+    for (int i = 1; i <= degreeB; i++)
+    {
+        degree = degree*NumberA;
+    }
+    Console.WriteLine(degree);
+}
+
