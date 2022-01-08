9edc9a5095b0085ace580c132cc9b0dfc49d6566
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-08 19:18:27 +0300

    Add solution Task027

---
+++ Task027/Program.cs
@@ -0,0 +1,19 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.WriteLine("Введите число");
+var a = Console.ReadLine();
+int count = 0;
+if (int.TryParse(a, out int number))
+{
+    for (int i = 0; number > 0; i++)
+    {
+        number = number / 10;
+        count++;
+    }
+    Console.WriteLine($"Во введенном числе {count} цифр");
+}
+
+else
+{
+    Console.WriteLine("error");
+}
