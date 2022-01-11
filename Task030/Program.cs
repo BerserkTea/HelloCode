23acbd5954dbb81bd126a3a354ec3e736f5cb264
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-11 14:15:55 +0300

    Add solution Task030

---
+++ Task030/Program.cs
@@ -0,0 +1,14 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+//выделить последнее число
+int LastNumber = N%10;
+if (LastNumber%2==0)
+{
+    N = N*N*N;
+    Console.WriteLine(N);
+}
+else
+{
+    Console.WriteLine("Число не заканчивается на четную цифру.");
+}
