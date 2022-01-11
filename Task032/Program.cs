e30c99a6fb778475ea11c7ac30291bcc081f16d2
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-11 15:28:35 +0300

    Add solution Task032

---
+++ Task032/Program.cs
@@ -0,0 +1,26 @@
+﻿//32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 2);
+        //index = index + 1;
+        index++;
+    }
+}
+void PrintArr(int[] cop)
+{
+    int count = cop.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(cop[position]);
+        position++;
+    }
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArr(array);
