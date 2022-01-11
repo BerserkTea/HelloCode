931ac3b7937c31e577ebc011f56d25a892fe8ee0
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-11 14:32:21 +0300

    Add solution Task031

---
+++ Task031/Program.cs
@@ -0,0 +1,10 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+int i = 0;
+while (i < array.Length)
+{
+    Console.WriteLine(array[i]);
+    i++;
+}
+
