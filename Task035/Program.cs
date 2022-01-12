aec7f150c44e45c84b85dacdca040fb5c400f4b5
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-12 21:06:50 +0300

    Add solution Task035

---
+++ Task035/Program.cs
@@ -0,0 +1,20 @@
+﻿// 35. Определить, присутствует ли в заданном массиве, некоторое число
+int [] somearray = {9,6,7,42,1,8,123,87,23,76,21412,6,0,12,42,42,123,7};
+
+void CheckArrayForNumber (int[] array)
+{
+    Console.WriteLine("Какое число искать?");
+    int search = Convert.ToInt32(Console.ReadLine());
+    int counter = 0;
+    for(int i =0; i<array.Length;i++)
+    {
+        if (array[i]==search)
+        {
+            Console.WriteLine($"{i}й член массива равный {array[i]} совпадает с заданным числом");
+            counter++;
+        }
+    
+    }
+Console.WriteLine($"В массиве найдено {counter} элементов совпадающих с числом {search}");
+}
+CheckArrayForNumber(somearray);
