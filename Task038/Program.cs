8ee80226f0c3675dffb54391cbbdca9ba30333f4
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-14 10:53:21 +0300

    Add solution Task038

---
+++ Task038/Program.cs
@@ -0,0 +1,11 @@
+﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int [] somearray = {1,2,3,4,5,6,7,8,9,7,6,10,12,15};
+int sum = 0;
+for (int i = 0; i < somearray.Length; i++)
+{
+    if (i%2==0)
+    {
+        sum=sum + somearray[i];
+    }
+}
+Console.WriteLine (sum);
