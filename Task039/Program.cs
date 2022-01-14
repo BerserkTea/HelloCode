6ee0e112836de95ddf153672bb9cf8d54823b331
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-14 11:12:10 +0300

    Add solution Task039

---
+++ Task039/Program.cs
@@ -0,0 +1,14 @@
+﻿// 39. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int[] somearray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 6, 10, 12, 15 };
+int[] secondsomearray = new int[(somearray.Length)/2];
+int fromBackCounter = somearray.Length - 1;
+
+for (int i = 0; i < secondsomearray.Length; i++)
+{
+    secondsomearray[i] = somearray[i] * somearray[fromBackCounter];
+    --fromBackCounter;
+}
+for (int j = 0; j < secondsomearray.Length; j++)
+{
+    Console.WriteLine(secondsomearray[j]);
+}
