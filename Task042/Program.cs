d01ee45129f6c428ad7fee75b0af128f4bea064e
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-16 18:38:12 +0300

    add new solution Task042

--- Task042/Program.cs
+++ Task042/Program.cs
@@ -1,43 +1,43 @@
 ﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
-// Console.WriteLine("Введи-ка мне дружок через пробел цифирь");
-// string stroka = line;
+Console.WriteLine("Введи-ка мне дружок через пробел цифирь");
+string stroka = Console.ReadLine();
 
-// string[] strokaArr = stroka.Split("");
+string[] strokaArr = stroka.Split(" ");
 
-// int counter = 0;
-// for (int i = 0; i < strokaArr.Length; i++)
-// {
-//     bool result = int.TryParse(strokaArr[i], out int number); // ввели переменную для проверки а число ли перед нами
-//     if (result == false)
-//     {
-//         Console.WriteLine("введено не число");    
-//     }
-//     else
-//     {
-//         if (number>0)
-//         {
-//              counter++;
-//         }      
-//       //Console.WriteLine(number);  использовал для проверки что  распределилилось в массив
-//     }
-// }
-// Console.WriteLine($"Хозяин, Доби сосчитал до {counter} подсчитывая цифры больше 0.");
-string line;
-int countPositive = 0;
-Console.WriteLine("Введите числа, для завершения введите quit");
-do
+int counter = 0;
+for (int i = 0; i < strokaArr.Length; i++)
 {
-    line = Console.ReadLine();
-    bool ResultOfParse = int.TryParse(line, out int number);
-    while (ResultOfParse = false)
+    bool result = double.TryParse(strokaArr[i], out double number); // ввели переменную для проверки а число ли перед нами
+    if (result == false)
     {
-        Console.WriteLine("Некорректный ввод, попробуйте снова.");
-        ResultOfParse = int.TryParse(line, out number);
+        Console.WriteLine("введено не число");    
     }
-    if (number > 0)
+    else
     {
-        countPositive++;
+        if (number>0)
+        {
+             counter++;
+        }      
+      //Console.WriteLine(number);  использовал для проверки что  распределилилось в массив
     }
+}
+Console.WriteLine($"Хозяин, Доби сосчитал до {counter} подсчитывая цифры больше 0.");
+// string line;
+// int countPositive = 0;
+// Console.WriteLine("Введите числа, для завершения введите quit");
+// do
+// {
+//     line = Console.ReadLine();
+//     bool ResultOfParse = int.TryParse(line, out int number);
+//     while (ResultOfParse = false)
+//     {
+//         Console.WriteLine("Некорректный ввод, попробуйте снова.");
+//         ResultOfParse = int.TryParse(line, out number);
+//     }
+//     if (number > 0)
+//     {
+//         countPositive++;
+//     }
 
-} while (line!="quit");
-Console.WriteLine(countPositive);
+// } while (line!="quit");
+// Console.WriteLine(countPositive);
