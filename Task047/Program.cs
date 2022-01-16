d8b65ddd47fce85c030ce62a1b6dc98f89313dd8
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-17 00:45:40 +0300

    Added solution Task047

---
+++ Task047/Program.cs
@@ -0,0 +1,20 @@
+﻿// 47. Написать программу копирования массива
+int[] someArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+int[] copyArryMethod(int[] anotherarray)
+{
+    int[] copyArray = new int[anotherarray.Length];
+    for (int i = 0; i < anotherarray.Length; i++)
+    {
+        copyArray[i] = anotherarray[i];
+    }
+    return copyArray;
+}
+void printArray(int[] whatToPrint)
+{
+    for (int i = 0; i < whatToPrint.Length; i++)
+    {
+        Console.Write($"{whatToPrint[i]} ");
+    }
+}
+printArray(copyArryMethod(someArray));
