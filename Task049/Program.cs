9d1fb3a81c84b0289d1c7972c3fd071e2f5c9a77
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-18 21:24:45 +0300

    added solution Task049

---
+++ Task049/Program.cs
@@ -0,0 +1,30 @@
+﻿// 49. Показать двумерный массив размером m×n заполненный вещественными числами
+Console.WriteLine("Введите число строк");
+int strings = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+void FillTwoDimensiounalArray(double[,] sometwodimensionalarray)
+{
+    for (int i = 0; i < sometwodimensionalarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < sometwodimensionalarray.GetLength(1); j++)
+        {
+            sometwodimensionalarray[i, j] = (new Random().Next(1, 99)+Math.Round((new Random().NextDouble()),2));
+        }
+    }
+}
+void PrintTwoDimensionalArray (double[,] anothertwodimensionalarray)
+{
+    for (int i = 0; i < anothertwodimensionalarray.GetLength(0); i++)
+    {
+        for (int j = 0; j < anothertwodimensionalarray.GetLength(1); j++)
+        {
+            Console.Write($"{anothertwodimensionalarray[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+double[,] array = new double[strings, columns];
+FillTwoDimensiounalArray(array);
+PrintTwoDimensionalArray(array);
