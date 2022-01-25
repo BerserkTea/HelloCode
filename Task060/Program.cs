2b83ff7a56b92b64195868be8963971193f368c2
«AndronovIvan» <andronoviv4@gmail.com>
2022-01-25 11:37:38 +0300

    added some fitches 1st try Task060

--- Task060/Program.cs
+++ Task060/Program.cs
@@ -1,6 +1,6 @@
 ﻿// 60. Составить частотный словарь элементов двумерного массива
 Console.Clear();
-int[,] matrix = new int[2,5];
+int[,] matrix = new int[20,50];
 void FillTwoDimensiounalArray(int[,] sometwodimensionalarray)
 {
     for (int i = 0; i < sometwodimensionalarray.GetLength(0); i++)
@@ -26,29 +26,29 @@ void PrintTwoDimensionalArray(int[,] anothertwodimensionalarray)
 
 int[,] FrequencyAnalisMatrix (int[,] freqmatrix)
 {
-int [,] resultArray = new int [freqmatrix.GetLength(0)*freqmatrix.GetLength(1),2];
-int placeNewArray = 0;
-for (int i = 0; i < freqmatrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < freqmatrix.GetLength(1); j++)
-            {
-                resultArray[placeNewArray,0] = freqmatrix[i,j];
-                int counterSubmission=0;
-                for (int k = 0; k < freqmatrix.GetLength(0); k++)
+    int [,] resultArray = new int [freqmatrix.GetLength(0)*freqmatrix.GetLength(1),2];
+    int placeNewArray = 0;
+    for (int i = 0; i < freqmatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < freqmatrix.GetLength(1); j++)
                 {
-                    for (int h = 0; h < freqmatrix.GetLength(1); h++)
+                    resultArray[placeNewArray,0] = freqmatrix[i,j];
+                    int counterSubmission=0;
+                    for (int k = 0; k < freqmatrix.GetLength(0); k++)
                     {
-                        if(freqmatrix[i,j] == freqmatrix[k,h])
+                        for (int h = 0; h < freqmatrix.GetLength(1); h++)
                         {
-                            counterSubmission++;
+                            if(freqmatrix[i,j] == freqmatrix[k,h])
+                            {
+                                counterSubmission++;
+                            }
                         }
                     }
+                    resultArray[placeNewArray,1] = counterSubmission;
+                    placeNewArray++;
                 }
-                resultArray[placeNewArray,1] = counterSubmission;
-                placeNewArray++;
-            }
-    }    
-return resultArray;
+        }    
+    return resultArray;
 }
 
 void RemoveRepetition (int[,] resultfreqmatrix)
@@ -62,13 +62,15 @@ void RemoveRepetition (int[,] resultfreqmatrix)
                 resultfreqmatrix[k,1] = (Math.Abs(resultfreqmatrix[k,1]))*(-1);
             }
         }
-    Console.WriteLine($"Проверка {resultfreqmatrix[i,0]} {resultfreqmatrix[i,1]} ");   
+    // Console.WriteLine($"Проверка {resultfreqmatrix[i,0]} {resultfreqmatrix[i,1]} ");   
     }
     for (int j = 0; j < resultfreqmatrix.GetLength(0); j++)
     {
         if(resultfreqmatrix[j,1]>0)
         {
-            Console.WriteLine($"Число {resultfreqmatrix[j,0]} встречается {resultfreqmatrix[j,1]} раз");
+            Console.Write($"Число {resultfreqmatrix[j,0]} встречается {resultfreqmatrix[j,1]} раз.");
+            double freq = (Convert.ToDouble(resultfreqmatrix[j,1])/Convert.ToDouble(resultfreqmatrix.GetLength(0))*100);
+            Console.WriteLine($"Частота {freq} процентов");
         }
     }
 }
