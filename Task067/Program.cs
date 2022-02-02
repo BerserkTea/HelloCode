91a4fe07b304823dcaa1a46906d22ab2acfe1794
«AndronovIvan» <andronoviv4@gmail.com>
2022-02-02 09:29:45 +0300

    added solution Task067

---
+++ Task067/Program.cs
@@ -0,0 +1,12 @@
+﻿// 67. Показать натуральные числа от N до 1, N задано
+
+int N = 25;
+
+void FromNToOne (int lastnumber)
+{
+    Console.Write($"{lastnumber} ");
+    if (lastnumber!= 1)
+    FromNToOne (lastnumber-1);
+  
+}
+FromNToOne(N);
