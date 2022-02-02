ed0d5253916e6af7501d895d0dbe59349cd9becb
«AndronovIvan» <andronoviv4@gmail.com>
2022-02-02 12:46:29 +0300

    added solution Task069

---
+++ Task069/Program.cs
@@ -0,0 +1,12 @@
+﻿// 69. Найти сумму элементов от M до N, N и M заданы
+
+int N = 13;
+int M = 12;
+int SummFromMToN (int firstnumber,int lastnumber)
+{
+    if (firstnumber== lastnumber) return lastnumber;
+    else return firstnumber += SummFromMToN(firstnumber+1,lastnumber);
+}
+
+int summary = SummFromMToN(M,N);
+Console.WriteLine (summary);
