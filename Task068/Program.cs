f3e1bb4c1a3985541b13b64264e44b8fb9b0893d
«AndronovIvan» <andronoviv4@gmail.com>
2022-02-02 09:46:48 +0300

    added solution Task068

---
+++ Task068/Program.cs
@@ -0,0 +1,11 @@
+﻿// 68. Показать натуральные числа от M до N, N и M заданы
+int N = 35;
+int M = 6;
+void FromMToN (int firstnumber,int lastnumber)
+{
+    Console.Write($"{firstnumber} ");
+    if (firstnumber!= lastnumber)
+    FromMToN (firstnumber+1,lastnumber);
+  
+}
+FromMToN(M,N);
