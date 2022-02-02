4ae26bec7311f4e926e788b0e91b0798e5f4a973
«AndronovIvan» <andronoviv4@gmail.com>
2022-02-02 09:42:05 +0300

    added solution Task066

---
+++ Task066/Program.cs
@@ -0,0 +1,12 @@
+﻿//66. Показать натуральные числа от 1 до N, N задано 
+
+int N = 25;
+int counter = 1;
+void FromOneToN (int lastnumber, int firstnumber)
+{
+    Console.Write($"{firstnumber} ");
+    if (firstnumber!= lastnumber)
+    FromOneToN (lastnumber,firstnumber+1);
+  
+}
+FromOneToN(N,counter);
